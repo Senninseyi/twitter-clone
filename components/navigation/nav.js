0d764dd73559aa1767fbe8b4605d7fec327b9c8d
Senninseyi <seyi.oyebamiji@gmail.com>
2022-12-14 20:27:30 +0100

    Create nav.js

---
+++ components/navigation/nav.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <div
+      className="absolute bottom-0 bg-primary w-full
+     left-0 flex px-6 py-4"
+    >
+      Nav
+    </div>
+  );
+};
+
+export { Nav };
