c62f5f952825cee1ccbf88ac0ff5e9d9441ac851
Senninseyi <seyi.oyebamiji@gmail.com>
2022-12-14 20:27:21 +0100

    Create logo.js

---
+++ components/logo/logo.js
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+export const Logo = () => {
+  return (
+    <>
+      <Link className="" href="/">
+        <div className="rounded-full bg-white w-fit p-2 hover:bg-blue-100
+         transition-all ease-out duration-1000">
+          <Image src="/Logoblue.svg" width={32} height={32} />
+        </div>
+      </Link>
+    </>
+  );
+};
