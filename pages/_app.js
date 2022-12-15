5b71fe2c5c1f69943f69c066ab4d456763a29d52
Senninseyi <seyi.oyebamiji@gmail.com>
2022-12-15 07:23:13 +0100

    chore: add theme provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import { ThemeProvider } from "next-themes";
 import { DarkMode } from "../components/dark";
 import Layout from "../components/layout/layout";
 import "../styles/globals.css";
@@ -5,12 +6,14 @@ import "../styles/globals.css";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <div className="absolute bottom-14 z-10 right-4">
-        <DarkMode />
-      </div>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <ThemeProvider attribute="class" enableSystem={true}>
+        <div className="absolute bottom-14 z-10 right-4">
+          <DarkMode />
+        </div>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
     </>
   );
 }
