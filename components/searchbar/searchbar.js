03623a041408a8d3f8cc2f92b0ce00acf683550f
Senninseyi <seyi.oyebamiji@gmail.com>
2022-12-15 09:03:19 +0100

    chore: add search icom

--- components/searchbar/searchbar.js
+++ components/searchbar/searchbar.js
@@ -8,11 +8,28 @@ const Searchbar = () => {
         className="w-full py-3 px-6 sticky dark:bg-white dark:bg-opacity-10
        dark:backdrop-blur-lg top-0 flex justify-center"
       >
-        <div className="flex w-full gap-4">
+        <div className="flex relative w-full gap-4">
+          <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg
+              aria-hidden="true"
+              class="w-5 h-5 text-gray-500 dark:text-gray-400"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              ></path>
+            </svg>
+          </div>
           <input
             type="search"
             placeholder="Search Twitter"
-            className="w-full px-4 py-2 rounded-2xl outline-none bg-primary
+            className="w-full pl-12 py-2 rounded-2xl outline-none bg-primary
              text-black dark:bg-white bg-opacity-10 placeholder:text-black text-sm font-medium"
           />
           <button>
