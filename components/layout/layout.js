d0118a475f0fbb59c4a81d138cb222bbda740df9
Senninseyi <seyi.oyebamiji@gmail.com>
2022-12-15 07:22:10 +0100

    style: darkmode bg

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -6,7 +6,9 @@ const Layout = ({ children, isAuth }) => {
     <div className="w-full h-full flex justify-center">
       <div className="max-w-[1440px] flex flex-col justify-center items-center w-full">
         {isAuth ? <Nav /> : isAuth}
-        <main className="w-full h-full bg-white">{children}</main>
+        <main className="w-full h-full bg-white dark:bg-black">
+          {children}
+        </main>
       </div>
     </div>
   );
