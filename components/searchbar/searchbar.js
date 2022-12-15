a6d04659475781eb3ade0961997623f691daa1ed
Senninseyi <seyi.oyebamiji@gmail.com>
2022-12-15 07:23:26 +0100

    Create searchbar.js

---
+++ components/searchbar/searchbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Searchbar = () => {
+  return (
+    <>
+        <div className='w-full py-3 px-6 fixed top-0'>Searchbar</div>
+    </>
+  )
+}
+
+export default Searchbar
