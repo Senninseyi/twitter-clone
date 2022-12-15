f8aa6a07a84de527dd9b82280f5504e7ad122b06
Senninseyi <seyi.oyebamiji@gmail.com>
2022-12-15 07:22:35 +0100

    Create data.js

---
+++ data/data.js
@@ -0,0 +1,31 @@
+import { Icon } from "@iconify/react";
+
+export const Links = [
+  {
+    link: "/explore",
+    icon: <Icon icon="fa-solid:hashtag" />,
+    label: "Explore",
+  },
+  {
+    link: "/settings",
+    icon: <Icon icon="ph:gear-fill" />,
+    label: "Settings",
+  },
+];
+
+export const authLinks = [
+  {
+    link: "",
+    icon: <Icon icon="flat-color-icons:google" />,
+    label: "Sign up with Google",
+  },
+  {
+    link: "",
+    icon: <Icon icon="logos:apple" />,
+    label: "Sign up with Apple",
+  },
+  {
+    link: "",
+    label: "Sign up with phone or email",
+  },
+];
