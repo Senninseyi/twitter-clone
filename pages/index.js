9a00afb27082dbe188b770f21f42ef3f73062e86
Senninseyi <seyi.oyebamiji@gmail.com>
2022-12-15 09:46:58 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import Layout from "../components/layout/layout";
 
@@ -5,8 +6,24 @@ export default function Home() {
   return (
     <>
       <Layout>
-        <div>Hello</div>
-        <Link href="/signin">Signin</Link>
+        <div className="w-full h-full min-h-screen bg-primary dark:bg-black">
+          <div className="w-full h-full flex flex-col gap-4 items-center justify-center">
+            <div className="">
+              <Image
+                src="/logowhite.svg"
+                width={250}
+                height={250}
+                alt="logo"
+                className="object-contain"
+              />
+            </div>
+            <div>
+              <Link href="/signin" className="text-white font-medium">
+                Sign in
+              </Link>
+            </div>
+          </div>
+        </div>
       </Layout>
     </>
   );
