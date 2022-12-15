37b91f1d0028fb428909eca310e15225d98c5535
Senninseyi <seyi.oyebamiji@gmail.com>
2022-12-15 06:20:01 +0100

    Update tailwind.config.js
    add primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
+  mode: "jit",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1D9BF0",
+      },
+    },
   },
   plugins: [],
 };
