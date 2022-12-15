0316d39825daf1142bd1ae948525f5f9d0525c5a
Senninseyi <seyi.oyebamiji@gmail.com>
2022-12-15 07:22:26 +0100

    chore: add alt to logo

--- components/logo/logo.js
+++ components/logo/logo.js
@@ -6,9 +6,11 @@ export const Logo = () => {
   return (
     <>
       <Link className="" href="/">
-        <div className="rounded-full bg-white w-fit p-2 hover:bg-blue-100
-         transition-all ease-out duration-1000">
-          <Image src="/Logoblue.svg" width={32} height={32} />
+        <div
+          className="rounded-full bg-white w-fit p-2 hover:bg-blue-100
+         transition-all ease-out duration-1000"
+        >
+          <Image src="/Logoblue.svg" width={32} height={32} alt="logo" />
         </div>
       </Link>
     </>
