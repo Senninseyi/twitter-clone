6e3965b15d5cc101e71914b0f65f6fcd4e2fd2a9
Senninseyi <seyi.oyebamiji@gmail.com>
2022-12-15 06:19:31 +0100

    Update _app.js
    add toggle darkmode button to all pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import { DarkMode } from "../components/dark";
+import Layout from "../components/layout/layout";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <div className="absolute bottom-14 z-10 right-4">
+        <DarkMode />
+      </div>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
