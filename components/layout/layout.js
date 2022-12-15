83a2d8736142a2de2fc39eaa897788bf227afca3
Senninseyi <seyi.oyebamiji@gmail.com>
2022-12-15 09:36:04 +0100

    Update layout.js

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -6,7 +6,7 @@ const Layout = ({ children, isAuth }) => {
     <div className="w-full h-full flex justify-center">
       <div className="max-w-[1440px] flex flex-col justify-center items-center w-full">
         {isAuth ? <Nav /> : isAuth}
-        <main className="w-full h-full bg-white dark:bg-black">
+        <main className="w-full h-full">
           {children}
         </main>
       </div>
