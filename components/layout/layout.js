7f60f95ef108afa00e1992062363ccd3ec5fa6d5
Senninseyi <seyi.oyebamiji@gmail.com>
2022-12-15 06:18:42 +0100

    style: fix max-width for larger devices

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -3,9 +3,11 @@ import { Nav } from "../navigation/nav";
 
 const Layout = ({ children, isAuth }) => {
   return (
-    <div className="w-full h-full">
-      {isAuth ? <Nav /> : isAuth}
-      <main className="w-full h-full bg-white">{children}</main>
+    <div className="w-full h-full flex justify-center">
+      <div className="max-w-[1440px] flex flex-col justify-center items-center w-full">
+        {isAuth ? <Nav /> : isAuth}
+        <main className="w-full h-full bg-white">{children}</main>
+      </div>
     </div>
   );
 };
